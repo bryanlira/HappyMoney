0dc99833b3ab469a9fb153a1e7e15bcd92900eee
bryan <bryanx5163@gmail.com>
2017-02-01 10:50:50 -0700

    Fixes with the arithmetic operations

--- lib/arithmetic.rb
+++ lib/arithmetic.rb
@@ -1,34 +1,41 @@
 module Arithmetic
   def *(value)
-    if value.instance_of? HappyMoney::Money
-      self.class.new(self.amount * value.convert_to(self.currency).amount, self.currency)
+    value = get_value(value)
+    if value.is_a? Numeric
+      self.class.new(self.amount * value, self.currency)
     else
       raise TypeError, "Can't multiply a #{self.class.name} by a #{value.class.name}'s value"
     end
   end
 
   def /(value)
-    if value.instance_of? HappyMoney::Money
-      self.class.new(self.amount / value.convert_to(self.currency).amount, self.currency)
+    value = get_value(value)
+    if value.is_a? Numeric
+      self.class.new(self.amount / value, self.currency)
     else
       raise TypeError, "#{self.class.name} can't divide by #{value.class.name}'s value"
     end
   end
 
   def +(value)
-    if value.instance_of? HappyMoney::Money
-      self.class.new(self.amount + value.convert_to(self.currency).amount, self.currency)
+    value = get_value(value)
+    if value.is_a? Numeric
+      self.class.new(self.amount + value, self.currency)
     else
       raise TypeError, "#{self.class.name} couldn't be added by #{value.class.name}'s value"
     end
   end
 
   def -(value)
-    if value.instance_of? HappyMoney::Money
-      self.class.new(self.amount - value.convert_to(self.currency).amount, self.currency)
+    value = get_value(value)
+    if value.is_a? Numeric
+      self.class.new(self.amount - value, self.currency)
     else
       raise TypeError, "#{self.class.name} couldn't be subtracted by #{value.class.name}'s value"
     end
   end
 
+  def get_value(value)
+    value.instance_of?(HappyMoney::Money) ? value.convert_to(self.currency).amount : value
+  end
 end
